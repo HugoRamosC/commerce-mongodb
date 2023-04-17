fd78837286b260e457e79f020228d82f6ba1f6c5
HugoRamosC <hugor.castilho@gmail.com>
2023-04-17 17:30:19 -0300

    feat: get percentual protein between 30 and 40 - challenge10

---
+++ challenges/desafio10.js
@@ -0,0 +1,11 @@
+db.produtos.find(
+  {
+    valoresNutricionais: {
+      $elemMatch: {
+        tipo: 'proteínas',
+        percentual: { $gt: 30, $lte: 40 },
+      }
+    }
+  },
+  { _id: 0, nome: 1 }
+);
